2108514b1d319f91bf8d3a8fba9143d447dd082b
204G1A0534 <94550650+204G1A0534@users.noreply.github.com>
2022-10-11 11:30:41 +0530

    P.Gousiya created Priority.c

---
+++ oslab/Priority.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: P. Gousiya
+   Roll No: 204G1A0534 
+   Title of the Experiment: CPU Scheduling Using Priority Algorithm
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
